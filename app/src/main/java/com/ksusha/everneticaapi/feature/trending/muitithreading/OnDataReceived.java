1e8c1d93929e9b5bc4424746080a71ba8ff2e6f2
Ксения Ладошко <87228073+KsushaLad@users.noreply.github.com>
2021-11-15 19:40:20 +0300

    Delete app/src/main/java/com/ksusha/everneticaapi directory

--- app/src/main/java/com/ksusha/everneticaapi/feature/trending/muitithreading/OnDataReceived.java
+++
@@ -1,9 +0,0 @@
-package com.ksusha.everneticaapi.feature.trending.muitithreading;
-
-import com.ksusha.everneticaapi.feature.model.ImageModel;
-
-import java.util.List;
-
-public interface OnDataReceived {
-    void onReceived(List<ImageModel> list);
-}
